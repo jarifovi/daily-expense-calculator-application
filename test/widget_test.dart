3b1a1b0dcb287f97117212ae93da0e99713cc6af
jarifovi <jarifovi7@gmail.com>
2026-08-24 23:47:59 +0600

    Add project configuration file: test/widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('App smoke test', (WidgetTester tester) async {
+    // Firebase requires initialization before running
+    // Run integration tests instead for full app testing
+    expect(true, isTrue);
+  });
+}
